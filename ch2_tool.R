a9367ce8fd44e260b5dab4bb0da68383872fc4a3
Natapone C <natapone@gmail.com>
2017-09-18 17:37:27 +0700

    add tools

---
+++ ch2_tool.R
@@ -0,0 +1,10 @@
+# Pairwise correlation
+library(quantmod)
+
+# Stocks ticker
+symbols <- c('AAPL','GOOG','MSFT')
+getSymbols(symbols)
+
+# filter_and_sort_symbols <- function(symbols) {
+#     
+# }
